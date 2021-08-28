52c83272867bba2b0972a9e592c78af93bc98c6c
Adebayo Tola <tpharell22@gmail.com>
2021-08-28 13:58:24 +0100

    fix: removed-google-api-key

--- example/src/App.js
+++ example/src/App.js
@@ -7,20 +7,13 @@ import useGoogleScript from './hooks/useScript'
 
 const App = () => {
 
-  const apiKey = 'AIzaSyCAUBAsDOcwwDCH3E63VAQ7W8Vn8iKcg18'
+  const apiKey = 'XXXXX'
   useGoogleScript(apiKey);
   
 
   return <Fragment>
-    {/* <Helmet>
-    <script async
-    src={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&callback=initMap`}>
-</script>
-    </Helmet> */}
     <ExampleComponent text="Create React Library Example 😄" />
     <PlaceAutocomplete/>
-    <div id="details"></div>
-    <div id="map"></div>
   </Fragment>
 }
 
