54dd667f45d60551b1c21a999a0a15ad38c6c38d
Adebayo Tola <tpharell22@gmail.com>
2021-08-28 13:16:57 +0100

    feature: location setup

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
-import { ExampleComponent } from 'react-google-places-autocomplete'
+import { ExampleComponent, PlaceAutocomplete } from 'react-google-places-autocomplete'
 import 'react-google-places-autocomplete/dist/index.css'
+// import { Helmet } from 'react-helmet'
+import useGoogleScript from './hooks/useScript'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+
+  const apiKey = 'AIzaSyCAUBAsDOcwwDCH3E63VAQ7W8Vn8iKcg18'
+  useGoogleScript(apiKey);
+  
+
+  return <Fragment>
+    {/* <Helmet>
+    <script async
+    src={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&callback=initMap`}>
+</script>
+    </Helmet> */}
+    <ExampleComponent text="Create React Library Example 😄" />
+    <PlaceAutocomplete/>
+    <div id="details"></div>
+    <div id="map"></div>
+  </Fragment>
 }
 
 export default App
